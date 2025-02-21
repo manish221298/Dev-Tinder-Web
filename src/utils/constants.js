7c35b68d6637565d1bc6ed3351523ea25154c235
manish221298 <iammnsh01@gmail.com>
2025-02-21 20:40:17 +0530

    api config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const baseUrl = "http://localhost:4001";
+// export const baseUrl = "http://localhost:4001"; // local url
+
+export const baseUrl = "/api";
 
 export const getToken = () => {
   return localStorage.getItem("token");
