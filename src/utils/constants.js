e4bd32c45c9a170ee5488febd3d4fcaabe7c3636
manish221298 <iammnsh01@gmail.com>
2025-02-21 21:23:50 +0530

    update url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // export const baseUrl = "http://localhost:4001"; // local url
 
-export const baseUrl = "/api";
+export const baseUrl = "http://51.20.122.54/api";
+// http://51.20.122.54/api/user/signin
 
 export const getToken = () => {
   return localStorage.getItem("token");
